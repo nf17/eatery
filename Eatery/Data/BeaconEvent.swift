016aa87225b1c42580e4c750eab796e548f59945
Daniel Li <dl743@cornell.edu>
2016-04-24 15:34:26 -0400

    Begin BeaconRequest model

--- Eatery/Data/BeaconEvent.swift
+++ Eatery/Data/BeaconEvent.swift
@@ -10,26 +10,10 @@ import Foundation
 import SwiftyJSON
 
 struct BeaconEvent {
-    
-    /// The primary key of this event
-    var id: Int
-    
     /// The User ID
     var userID: Int
     
-    /// Name of this event
-    var title: String
-    
-    /// The date of the Beacon
-    var date: NSDate
-    
-    init(json: JSON) {
-        id = json[API.EventId].intValue
-        userID = json[API.UserId].intValue
-        title = json[API.EventTitle].stringValue
-        
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = APIDateFormat
-        date = formatter.dateFromString(json[API.EventDate].stringValue)!
-    }
+//    init(json: JSON) {
+//        
+//    }
 }
---
+++ Eatery/Data/BeaconRequest.swift
@@ -0,0 +1,14 @@
+//
+//  BeaconRequest.swift
+//  Eatery
+//
+//  Created by Daniel Li on 4/24/16.
+//  Copyright © 2016 CUAppDev. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct BeaconRequest {
+    
+}
